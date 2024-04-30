91e869ad94c27081e74f14ba7ec6541439ba531d
UrquizaN <urquiza-nobrega@outlook.com>
2024-04-30 06:41:43 -0300

    feat: create users json to handle api responses

---
+++ lib/banana_bank_web/controllers/users_json.ex
@@ -0,0 +1,31 @@
+defmodule BananaBankWeb.UsersJSON do
+  alias BananaBank.Users.User
+
+  def index(%{users: users}) do
+    %{
+      data: Enum.map(users, &data/1)
+    }
+  end
+
+  def create(%{user: user}) do
+    %{
+      message: "User created successfully",
+      data: data(user)
+    }
+  end
+
+  def delete(%{user: user}), do: %{message: "User deleted successfully", data: data(user)}
+
+  def show(%{user: user}), do: %{data: data(user)}
+
+  def update(%{user: user}), do: %{message: "User updated successfully", data: data(user)}
+
+  defp data(%User{} = user) do
+    %{
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      cep: user.cep
+    }
+  end
+end
