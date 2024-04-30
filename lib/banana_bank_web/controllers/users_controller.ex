2059d57c3e93a9a389939c12c82566c4099b642b
UrquizaN <urquiza-nobrega@outlook.com>
2024-04-30 06:40:55 -0300

    feat: create users controller

---
+++ lib/banana_bank_web/controllers/users_controller.ex
@@ -0,0 +1,48 @@
+defmodule BananaBankWeb.UsersController do
+  use BananaBankWeb, :controller
+
+  alias BananaBank.Users
+  alias Users.User
+
+  action_fallback BananaBankWeb.FallbackController
+
+  def index(conn, _params) do
+    with {:ok, users} <- Users.index() do
+      conn
+      |> put_status(:ok)
+      |> render(:index, users: users)
+    end
+  end
+
+  def create(conn, params) do
+    with {:ok, %User{} = user} <- Users.create(params) do
+      conn
+      |> put_status(:created)
+      |> render(:create, user: user)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    with {:ok, %User{} = user} <- Users.delete(id) do
+      conn
+      |> put_status(:ok)
+      |> render(:delete, user: user)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    with {:ok, %User{} = user} <- Users.show(id) do
+      conn
+      |> put_status(:ok)
+      |> render(:show, user: user)
+    end
+  end
+
+  def update(conn, params) do
+    with {:ok, %User{} = user} <- Users.update(params) do
+      conn
+      |> put_status(:ok)
+      |> render(:update, user: user)
+    end
+  end
+end
