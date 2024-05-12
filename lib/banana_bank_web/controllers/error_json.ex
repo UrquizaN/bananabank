26f8955ccd3a996ea30d0462ec2ce7c326d0b010
UrquizaN <urquiza-nobrega@outlook.com>
2024-05-12 20:57:37 -0300

    feat: add error json with only message

--- lib/banana_bank_web/controllers/error_json.ex
+++ lib/banana_bank_web/controllers/error_json.ex
@@ -27,6 +27,10 @@ defmodule BananaBankWeb.ErrorJSON do
     %{errors: Ecto.Changeset.traverse_errors(changeset, &translate_error/1)}
   end
 
+  def error(%{message: message}) do
+    %{message: message}
+  end
+
   def translate_error({msg, opts}) do
     Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
       opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
